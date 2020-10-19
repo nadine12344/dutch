ee6d4dc4bd9e277cdd181393d0b4599e710f75f4
nadinemoh <nadine.hassanhassan@student.guc.edu.eg>
2020-10-19 09:51:23 +0200

    second commit

--- DutchTreat/wwwroot/js/index.js
+++ DutchTreat/wwwroot/js/index.js
@@ -1,13 +1,21 @@
-﻿var x = 0;
-var s = "";
-var theForm = $("#theForm");
-theForm.hide();
+﻿$(document).ready(function () {
+	var x = 0;
+	var s = "";
+	var theForm = $("#theForm");
+	theForm.hide();
 
-var button = $("#buyButton");
-button.on("click", function () {
-	console.log("buying");
-});
-var productInfo = $(".products li");
-productInfo.on("click", function () {
-	console.log("you clicked on " + $(this).text());
+	var button = $("#buyButton");
+	button.on("click", function () {
+		console.log("buying");
+	});
+	var productInfo = $(".products li");
+	productInfo.on("click", function () {
+		console.log("you clicked on " + $(this).text());
+	}); 
+	var $loginToggle = $("#loginToggle");
+	var $popup = $(".popup");
+	$loginToggle.on("click", function () {
+		$popup.fadeIntoggle(1000);
+	});
+
 });
